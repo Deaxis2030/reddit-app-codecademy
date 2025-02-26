eac935ec3107d0d906990c48387a3aad298dc45a
Deaxis <malcolmnoel2030@gmail.com>
2025-02-26 09:39:30 -0500

    Included fallback for Video url and a general fallback for panelData in panel.js to help with error handling

--- src/Components/Sidebar/sidebar.js
+++ src/Components/Sidebar/sidebar.js
@@ -1,60 +1,61 @@
 import { React, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { isLoadingSubreddits, failedToLoadSubreddits, listOfSubreddits, loadSubreddits } from "./SidebarSlice";
+import {
+  isLoadingSubreddits,
+  failedToLoadSubreddits,
+  listOfSubreddits,
+  loadSubreddits,
+} from "./SidebarSlice";
 import { loadPosts } from "../panel/panelSlice";
 import OneBar from "./onebar";
-import styles from "../../Features/App.module.css"
+import styles from "../../Features/App.module.css";
 
 //Start of Side Bar function displays a list of most popular subreddits
 export default function SideBar() {
+  //Main Variables
+  const [button, setButton] = useState(false);
+  const loading = useSelector(isLoadingSubreddits);
+  const failed = useSelector(failedToLoadSubreddits);
+  const subreddits = useSelector(listOfSubreddits);
+  const dispatch = useDispatch();
+  const loadingText = <p className={styles.loadingText}>Loading Posts...</p>;
+  const failedText = <p className={styles.failedText}>Failed to load Posts!</p>;
 
-    //Main Variables
-    const [button, setButton] = useState(false);
-    const loading = useSelector(isLoadingSubreddits);
-    const failed = useSelector(failedToLoadSubreddits);
-    const subreddits = useSelector(listOfSubreddits);
-    const dispatch = useDispatch();
-    const loadingText = <p className={styles.loadingText}>Loading Posts...</p>;
-    const failedText = <p className={styles.failedText}>Failed to load Posts!</p>
+  //Function to load subreddits
+  useEffect(() => {
+    dispatch(loadSubreddits());
+  }, [dispatch]);
 
-    //Function to load subreddits
-    useEffect (() => {
-        dispatch(loadSubreddits());
-    } , [dispatch])
+  //Function to load posts from url of subreddit clicked
+  const getUrl = (arg) => {
+    dispatch(loadPosts(arg));
+  };
 
-    //Function to load posts from url of subreddit clicked
-    const getUrl = (arg) => {
-        dispatch(loadPosts(arg));
-    }
+  const handleClick = (e) => {
+    e.preventDefault();
+    setButton(!button);
+  };
 
-    const handleClick = (e) => {
-        e.preventDefault();
-        setButton(!button);
-    }
-
-    //Return Section Mapping the subreddits to sidebar section
-    return (
-        <div className={button? styles.outerContainer: styles.noMarginContainer}>
-             <div className={button? styles.sideBarContainer: styles.noSideBar}>
-            <h2>Most Popular SubReddits</h2>
-            <div className={styles.sideBar}>
-                    {loading ? (
-                        `${loadingText}`
-                    )   : failed ? (
-                        `${failedText}`
-                    ) :
-                    (subreddits.length === 0)? "" : subreddits.map((child, index) => 
-                        <OneBar getUrl={getUrl} data={child.data} key={index}/>       
-                    ) 
-                    }
-            </div>
-          
-        </div>  
-            <button 
-                className={styles.sideBarButton}
-                onClick={handleClick}    
-            >
-                press</button>
+  //Return Section Mapping the subreddits to sidebar section
+  return (
+    <div className={button ? styles.outerContainer : styles.noMarginContainer}>
+      <div className={button ? styles.sideBarContainer : styles.noSideBar}>
+        <h2>Most Popular SubReddits</h2>
+        <div className={styles.sideBar}>
+          {loading
+            ? `${loadingText}`
+            : failed
+            ? `${failedText}`
+            : subreddits.length === 0
+            ? ""
+            : subreddits.map((child, index) => (
+                <OneBar getUrl={getUrl} data={child.data} key={index} />
+              ))}
         </div>
-    )
+      </div>
+      <button className={styles.sideBarButton} onClick={handleClick}>
+        press
+      </button>
+    </div>
+  );
 }
--- src/Components/panel/panel.js
+++ src/Components/panel/panel.js
@@ -1,97 +1,114 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { useState } from "react";
-import ReactMarkdown from "react-markdown"
+import ReactMarkdown from "react-markdown";
 import CommentPanel from "../Comments/comments";
 import { listOfComments } from "../Comments/commentsSlice";
-import styles from "../../Features/App.module.css"
-import downArrow from "../../Features/Resources/Reddit App down Arrow.png"
-import upArrow from "../../Features/Resources/Reddit App Up Arrow.png"
+import styles from "../../Features/App.module.css";
+import downArrow from "../../Features/Resources/Reddit App down Arrow.png";
+import upArrow from "../../Features/Resources/Reddit App Up Arrow.png";
 import kNumberFormatter from "../../Features/Converters/kNumberFormatter";
 import dateFormatter from "../../Features/Converters/dateFormatter";
 import VideoPlayer from "../../Features/VideoPlayer/VideoPlayer";
 
 //Start of Panel Function whcih displays the data for a singular post
-export default function Panel (props){
+export default function Panel(props) {
+  //Main Variables
+  const { data, getCommentsData } = props;
+  const [button, setButton] = useState(false);
+  const commentsData = useSelector((state) => listOfComments(state, data.id));
+  let panelData;
 
-    //Main Variables
-    const {data, getCommentsData} = props;
-    const [button, setButton] = useState(false);
-    const commentsData = useSelector(state => listOfComments(state, data.id));
-    let panelData;
-   
-   //Try catch block for assigning the post data to panelData variable depending on the type of data being posted
-   try {
-    if(data.post_hint === "image") {
-        panelData = <img className={styles.onePanelIMG} src={data.url}></img>;
-    }   else if (data.post_hint === "hosted:video") {
-        const videoUrl = data.media?.reddit_video?.dash_url;
-        panelData = <VideoPlayer className={styles.onePanelVideo} url={videoUrl}/>
+  //Try catch block for assigning the post data to panelData variable depending on the type of data being posted
+  try {
+    if (data.post_hint === "image") {
+      panelData = (
+        <img className={styles.onePanelIMG} src={data.url} alt="Post Image" />
+      );
+    } else if (data.post_hint === "hosted:video") {
+      const videoUrl = data.media?.reddit_video?.dash_url;
+      if (videoUrl) {
+        panelData = (
+          <VideoPlayer className={styles.onePanelVideo} url={videoUrl} />
+        );
+      } else {
+        // Fallback if videoUrl is not available
+        panelData = <p>Video cannot be played. Please try again later.</p>;
+      }
     } else if (data.thumbnail === "self") {
-        panelData = <ReactMarkdown>{data.selftext}</ReactMarkdown>
+      panelData = <ReactMarkdown>{data.selftext}</ReactMarkdown>;
     } else if (data.post_hint === "link") {
-        panelData = <a className={styles.onePanelLink} href={data.url} target="_blank">{data.url}</a>
+      panelData = (
+        <a
+          className={styles.onePanelLink}
+          href={data.url}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {data.url}
+        </a>
+      );
     } else {
-        panelData = <a className={styles.onePanelLink} href={data.url} target="_blank">{data.url}</a>
+      panelData = (
+        <a
+          className={styles.onePanelLink}
+          href={data.url}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {data.url}
+        </a>
+      );
     }
-   } catch (err) {
+  } catch (err) {
     console.log(err);
-   }
+    // General fallback in case of unexpected errors
+    panelData = <p>Content cannot be displayed. Please try again later.</p>;
+  }
 
-   //HandleClick function that displays the comment panel
-   const handleClick = (e) => {
+  //HandleClick function that displays the comment panel
+  const handleClick = (e) => {
     e.preventDefault();
     setButton(!button);
     getCommentsData(data.subreddit_name_prefixed, data.id);
-    
-   };
+  };
 
-   //Return section detailing each singular panel
-    return ( 
-        <div>
-            <div className={styles.onePanelContainer}>  
-               <div className={styles.panelInfoContainer}>
-                    <div className={styles.subredditNameContainer}>
-                        <p>{data.subreddit_name_prefixed}</p>
-                    </div>
-                    <div className={styles.arrowContainer} >   
-                        <img className={styles.upArrow} src={upArrow}></img>
-                        <p>{kNumberFormatter(data.ups)}</p>
-                        <img className={styles.downArrow} src={downArrow} /> 
-                    </div>
-                    <div className={styles.authorContainer}>
-                        <p>by: {data.author}</p>
-                    </div>
-               </div>
-                
-                <div    
-                    className={styles.onePanel}
-                    key={data.id}
-                >
-                   <div className={styles.onePanelTitle}>
-                         <h3>{data.title}</h3>
-                   </div>
-                   <div className={styles.onePanelData}>
-                         {panelData}
-                   </div>
-                   <div className={styles.onePanelPostDate}>
-                        <h6>{dateFormatter(data)}</h6> 
-                   </div>
-                </div> 
-               
-                <div className={styles.buttonContainer}>  
-                    <button 
-                        className={styles.commentsBTN}
-                        onClick={handleClick}
-                    >
-                           {!button? "Comments": "Hide Comments"}
-                    </button>
-                </div>
-            </div>
-         
-            <CommentPanel button={button} data={data} comments={commentsData} />
-       
+  //Return section detailing each singular panel
+  return (
+    <div>
+      <div className={styles.onePanelContainer}>
+        <div className={styles.panelInfoContainer}>
+          <div className={styles.subredditNameContainer}>
+            <p>{data.subreddit_name_prefixed}</p>
+          </div>
+          <div className={styles.arrowContainer}>
+            <img className={styles.upArrow} src={upArrow}></img>
+            <p>{kNumberFormatter(data.ups)}</p>
+            <img className={styles.downArrow} src={downArrow} />
+          </div>
+          <div className={styles.authorContainer}>
+            <p>by: {data.author}</p>
+          </div>
         </div>
-    )
-}
 
+        <div className={styles.onePanel} key={data.id}>
+          <div className={styles.onePanelTitle}>
+            <h3>{data.title}</h3>
+          </div>
+          <div className={styles.onePanelData}>{panelData}</div>
+          <div className={styles.onePanelPostDate}>
+            <h6>{dateFormatter(data)}</h6>
+          </div>
+        </div>
+
+        <div className={styles.buttonContainer}>
+          <button className={styles.commentsBTN} onClick={handleClick}>
+            {!button ? "Comments" : "Hide Comments"}
+          </button>
+        </div>
+      </div>
+
+      <CommentPanel button={button} data={data} comments={commentsData} />
+    </div>
+  );
+}
